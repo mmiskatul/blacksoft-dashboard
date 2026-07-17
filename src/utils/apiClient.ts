eede794f08c7e247164399b12d30281c0b300254
mdnaeemmiah <153842332+mdnaeemmiah@users.noreply.github.com>
2026-07-17 17:48:06 +0600

    updated env

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,6 +1,6 @@
 'use client';
 
-const DEFAULT_API_BASE_URL = 'https://blacksoft-backend.vercel.app/api';
+const DEFAULT_API_BASE_URL = 'https://api.blacksoft.site/api';
 
 export type UploadResponse = {
   url: string;
